71796163133740818cc1d5e35bfbd3f0ef300ed8
Justin Bogner <im@justinbogner.com>
2007-12-12 21:46:12 -0700

    nice and stable

--- xmonad.hs
+++ xmonad.hs
@@ -1,21 +1,17 @@
 import XMonad
 import qualified XMonad.StackSet as W
 import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.ManageDocks
 --import XMonad.Layout.PerWorkspace
-
-import qualified Data.Map as M
 
 main = xmonad bogConfig
 
 bogConfig = defaultConfig
-            { keys        = \c -> keys' `M.union`
-                            keys defaultConfig c
-            , layoutHook  = layoutHook'
+            { defaultGaps = [(24,0,0,0)]
             , logHook     = logHook'
             , manageHook  = manageHook'
             , modMask     = mod4Mask
             , terminal    = "urxvtcd"
+            , layoutHook  = layoutHook'
             }
 
 logHook' = do ewmhDesktopsLogHook
@@ -25,19 +21,17 @@ manageHook' = composeAll
               [ className =? "Gimp"           --> doFloat
               , resource  =? "desktop_window" --> doIgnore
               , resource  =? "kdesktop"       --> doIgnore
-              ] <+> manageDocks <+> doF W.swapDown
+              , resource  =? "gnome-panel"    --> doIgnore
+              , resource  =? "kicker"         --> doIgnore
+              ] <+> doF W.swapDown
 
 -- workspaces' = ["web", "dev", "chat" ] ++ map show [4..9]
 
 layoutHook' =
 --    onWorkspace "1" (bigTiled ||| Full) $
-    avoidStruts (tiled ||| bigTiled ||| Mirror tiled ||| Full)
+    tiled ||| bigTiled ||| Mirror tiled ||| Full
   where
      tiled    = Tall nmaster delta (1/2)
      bigTiled = Tall nmaster delta (11/16)
      nmaster = 1
      delta   = 3/100
-
-keys' = M.fromList $
-    [ ((mod4Mask, xK_b), sendMessage ToggleStruts)
-    ]
