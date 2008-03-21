4ff3248560ac81406007f390a650786f42fce0a9
Justin Bogner <justin@justinbogner.com>
2008-03-21 09:47:02 -0600

    generic run or raise

--- xmonad.hs
+++ xmonad.hs
@@ -13,7 +13,9 @@ import XMonad.Prompt
 import XMonad.Prompt.Shell
 
 import XMonad.Actions.WindowGo
+import XMonad.Util.Run
 import Data.Maybe
+import Control.Monad
 
 import qualified Data.Map as M
 import Data.Bits ((.|.))
@@ -109,9 +111,20 @@ runOrRaisePrompt :: XPConfig -> X ()
 runOrRaisePrompt c = do cmds <- io $ getCommands
                         mkXPrompt RRP c (getShellCompl cmds) action
     where action = uncurry runOrRaise . getTarget
-          getTarget x = (,) x . fromMaybe (Query $ return False) . lookup x $ choices
-          choices = [ ("firefox", (className =? "Firefox-bin"))
-                    , ("emacs", (className =? "Emacs"))
-                    , ("pidgin", (className =? "Pidgin"))
-                    , ("thunderbird", (className =? "Thunderbird-bin"))
-                    ]
+          getTarget x = (x,isApp x)
+
+isApp :: String -> Query Bool
+isApp "firefox" = className =? "Firefox-bin"
+isApp "thunderbird" = className =? "Thunderbird-bin"
+isApp x = liftM2 (==) pid $ pidof x
+
+pidof :: String -> Query Int
+pidof x = io $ (runProcessWithInput "pidof" [x] [] >>= readIO) `catch` (\e -> return $ 0)
+
+pid :: Query Int
+pid = ask >>= (\w -> liftX $ withDisplay $ \d -> getPID d w)
+    where getPID d w = getAtom "_NET_WM_PID" >>= \a -> io $
+                       getWindowProperty32 d a w >>= return . getPID'
+          getPID' (Just (x:xs)) = fromIntegral x
+          getPID' (Just [])     = -1
+          getPID' (Nothing)     = -1
